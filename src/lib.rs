259442127bebcb341d4b063de2e44582edb33d51
ryan kurte <ryankurte@gmail.com>
2022-01-01 11:03:58 +1300

    ci, fix tool install names

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 //! Rust library wrapper for wasme/wasm3 API.
-//! Provided for testing via embedded-wasm-rt, not intended for consumer use
+//! 
+//! This may be prefered to rt_wasmtime for embedded device use
 
 #![no_std]
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals, clippy::all)]
